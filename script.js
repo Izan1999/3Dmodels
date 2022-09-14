ac2503a0b54cbcad13c20a4351baeb826f8fe4c9
Izan1999 <108541036+Izan1999@users.noreply.github.com>
2022-09-15 01:13:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function abrirmenu(){
+  document.getElementById("menu").style.marginLeft = "0";
+}
+
+function cerrarmenu(){
+  document.getElementById("menu").style.marginLeft = "-100%"
+}
+
+function cambiaAraña(){
+  
+  document.getElementById("scene").src = "3dmodels/spider.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaAk47(){
+
+  document.getElementById("scene").src = "3dmodels/ak47.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaSniper() {
+
+  document.getElementById("scene").src = "3dmodels/sniper.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaCubone() {
+
+  document.getElementById("scene").src = "3dmodels/cubone.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaPikachu() {
+
+  document.getElementById("scene").src = "3dmodels/pikachu.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaTierra() {
+
+  document.getElementById("scene").src = "3dmodels/tierra.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaTelefono() {
+
+  document.getElementById("scene").src = "3dmodels/telefono.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaDaga() {
+
+  document.getElementById("scene").src = "3dmodels/daga.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaSwitch() {
+
+  document.getElementById("scene").src = "3dmodels/Switch.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
+
+function cambiaTrex() {
+
+  document.getElementById("scene").src = "3dmodels/trex.glb";
+  document.getElementById("menu").style.marginLeft = "-100%";
+}
